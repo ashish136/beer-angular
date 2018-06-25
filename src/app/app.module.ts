cf7b24702044b306aaecd72b0db2f45eb7c20705
ashish-sharma <ashish.136sharma@gmail.com>
2018-06-25 18:02:39 +0530

    added some css in nav and ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,29 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { SideBarComponent } from "./side-bar/side-bar.component";
 import { BeerListComponent } from "./beer-list/beer-list.component";
-import { MyBeersComponent } from './my-beers/my-beers.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { FilterBeerPipe } from './filter-beer.pipe';
-import {FormsModule} from '@angular/forms';
-import { SortBeerPipePipe } from './sort-beer-pipe.pipe';
+import { MyBeersComponent } from "./my-beers/my-beers.component";
+import { AppRoutingModule } from ".//app-routing.module";
+import { FilterBeerPipe } from "./filter-beer.pipe";
+import { FormsModule } from "@angular/forms";
+import { SortBeerPipePipe } from "./sort-beer-pipe.pipe";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
 @NgModule({
-  declarations: [AppComponent, SideBarComponent, BeerListComponent, MyBeersComponent, FilterBeerPipe, SortBeerPipePipe],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule,FormsModule],
+  declarations: [
+    AppComponent,
+    SideBarComponent,
+    BeerListComponent,
+    MyBeersComponent,
+    FilterBeerPipe,
+    SortBeerPipePipe
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
